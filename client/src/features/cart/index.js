846b54bcb331e37614b580497416ba6b54d1e557
Sabeen Zehra <38291585+Syeda03@users.noreply.github.com>
2018-10-14 14:47:41 -0500

    Update index.js

--- client/src/features/cart/index.js
+++ client/src/features/cart/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { connect } from "react-redux"
 
-<<<<<<< cheryBranch
+
 function sort(items) {
     return items.sort((a,b) => a.id < b.id)
 }
@@ -75,15 +75,4 @@ export default connect(mapStateToProps, mapDispatchToProps)(Cart)
 
 
 
-// import React from "react"
-// import { connect } from "react-redux"
 
-// export const cartItemsWithQuantity = (cartItems) => {
-//     return cartItems.reduce((acc, item) => {
-//         const filteredItem = acc.filter(item2 => item2.id === item.id) [0]
-//         filteredItem !== undefined
-//             ? filteredItem.quantity++
-//             : acc.push({...item, quantity: 1 })
-//         return acc
-//     }, [])
-// }
