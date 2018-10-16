9060847dc61cf727c631be219ffde4be46dc215a
Sabeen Zehra <38291585+Syeda03@users.noreply.github.com>
2018-10-15 21:33:41 -0500

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
-import cartReducer from "../features/cart/reducer"
+import cartReducer from "./cartReducer"
 
 export default combineReducers({
   auth: authReducer,
