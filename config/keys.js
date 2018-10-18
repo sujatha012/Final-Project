25f791130b113d9e521c2eca72c590fc447313cc
syeda zehra <zehrasyeda@gmail.com>
2018-10-18 00:31:55 -0500

    prepared for deployment

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
-module.exports = {
-    mongoURI: 
-    "mongodb://sabeen:123456a@ds131763.mlab.com:31763/wrappingapp",
-    secretOrKey: "secret"
-}
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+  } else {
+    module.exports = require('./keys_dev');
+  }
+  
---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 
+  "mongodb://sabeen:123456a@ds131763.mlab.com:31763/wrappingapp",
+  secretOrKey: "secret"
+}
